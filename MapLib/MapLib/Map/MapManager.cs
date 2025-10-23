c4cf1053da8e8bd8090f4ddc7c6690ef80d844ed
anyhum <anyhum@yandex.ru>
2025-10-23 17:06:23 +0500

    GenerateMap

--- MapLib/MapLib/Map/MapManager.cs
+++ MapLib/MapLib/Map/MapManager.cs
@@ -13,18 +13,18 @@ namespace MapLib
 		
 		private const int _count = _width * _height;
 		
+		private const int _minTerritoryCount = 2;
+		private const int _maxTerritoryCount = 1000;
+
 		private static readonly long[] _tiles = new long[_count];
 		
-		private static readonly Dictionary<int, TerritoryInfo> _territories = [];
+		private static readonly Dictionary<int, TerritoryInfo> _territories = new(_maxTerritoryCount);
 		
 		/// <summary>
 		/// Генерация карты-заглушки для PoC
 		/// </summary>
 		public static void GenerateMap(int territoryCount = 100)
 		{
-			const int _minTerritoryCount = 2;
-			const int _maxTerritoryCount = 1000;
-			
 			territoryCount = Math.Max(territoryCount, _minTerritoryCount);
 			territoryCount = Math.Min(territoryCount, _maxTerritoryCount);
 			
@@ -34,15 +34,16 @@ namespace MapLib
 			
 			Span<long> tilesSpan = _tiles;
 			
-			for(int i=0; i<tilesSpan.Length; ++i)
+			for(int i=0; i<_count; ++i)
 			{
 				var type = (TileType) random.Next(0, 2); // Plain или Mountain
 				tilesSpan[i] = TileEncoder.Encode(type, 0); // ID территории пока 0
 			}
 			
-			// 2. Создание словаря территорий
+			// Создание словаря территорий
 			_territories.Clear();
-			for (int t = 1; t <= territoryCount; t++)
+
+			for(int t=1; t<=territoryCount; ++t)
 			{
 				_territories[t] = new TerritoryInfo
 				{
@@ -50,20 +51,20 @@ namespace MapLib
 					Name = $"Territory {t}",
 				};
 			}
-			
-			// 3. Простейшее распределение территорий по карте
+
+			// Распределение территорий по карте
+
 			int blockWidth = _width / territoryCount;
 			int blockHeight = _height / territoryCount;
 			
-			for (int y = 0; y < _height; y++)
+			for(int y = 0; y < _height; y++)
 			{
-				for (int x = 0; x < _width; x++)
+				for(int x = 0; x < _width; x++)
 				{
 					int territoryId = ((x / blockWidth) % territoryCount) + 1;
 					int index = GetIndex(x, y);
 					var type = tilesSpan[index].GetTileType();
 					tilesSpan[index] = TileEncoder.Encode(type, territoryId);
-					_territories[territoryId].TileCount++;
 				}
 			}
 		}
